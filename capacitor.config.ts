ef435136fbe82b1c5c960b1f3eacbc4f9379ac82
devibala04 <devibala.zerosoft@gmail.com>
2025-12-09 15:30:24 +0530

    Added Splash & App Icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'RealTimeDataCollector',
-  webDir: 'www'
+  appId: "io.ionic.starter",
+  appName: "RealTimeDataCollector",
+  webDir: "www",
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#ffffff",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+    },
+  },
 };
 
 export default config;
