acc382c6a4cc2b93d9a5ac38486d7de54d60eabc
Maharajazerosoft <maharajas.zerosoft@gmail.com>
2025-12-09 17:35:14 +0530

    Add: implement the home page

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'RealTimeDataCollector',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    StatusBar: {
+      style: 'Dark', // or 'Light'
+      overlaysWebView: false, // Important: set to false
+      backgroundColor: '#1ac1ee'
+    }
+  }
 };
 
 export default config;
